1f4d90b88da2f8df0d14546d1a936f99ca071028
2imClay <94099961+2imClay@users.noreply.github.com>
2023-12-23 15:33:31 +0700

    update

--- src/main/java/vn/edu/hcmuaf/fit/controller/Login.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/Login.java
@@ -9,7 +9,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "Login", value = "/doLogin")
-public class Login extends HttjjyyjServlet {
+public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
